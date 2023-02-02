ca7f35f46149b4b0c33e4d7ec228498c291e5332
Chandravo <chandravob2003@gmail.com>
2023-02-02 16:24:51 +0530

    added qr

--- test1/views.py
+++ test1/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.contrib.gis.geoip2 import GeoIP2
 import requests
+import qrcode
+import cloudinary, cloudinary.uploader, cloudinary.api
+config=cloudinary.config(secure=True)
+
+
 
 # Create your views here.
 def test1(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')    
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
+    # x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')    
+    # if x_forwarded_for:
+    #     ip = x_forwarded_for.split(',')[0]
+    # else:
+    #     ip = request.META.get('REMOTE_ADDR')
     
     # device_type = ""
     # browser_type = ""
@@ -26,12 +31,18 @@ def test1(request):
     # browser_version = request.user_agent.browser.version_string
     # os_type = request.user_agent.os.family
     # os_version = request.user_agent.os.version_string
-    # ip = '117.203.246.41'
+    ip = '117.203.246.41'
     ip_response = requests.get('http://ip-api.com/json/'+ip)  
     ip_response = ip_response.json()
     location_country = ip_response['country']
     location_region = ip_response['regionName']
     location_city = ip_response['city']
+       
+    
+    qr=qrcode.make('http://www.google.com')
+    cloudinary.uploader.upload(qr, public_id="hehe_qr", unique_filename = False, overwrite=True, folder="TagIt")
+    srcURL = cloudinary.CloudinaryImage("hehe_qr").build_url()
+    
     context = {
         "ip": ip,
         # "device_type": device_type,
@@ -41,8 +52,9 @@ def test1(request):
         # "os_version":os_version,
         "location_country": location_country,
         "location_region": location_region,
-        "location_city": location_city
-    }    
+        "location_city": location_city,
+        "srcURL": srcURL
+    } 
     
     return render(request,'1.html',context)
 
